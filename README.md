90521571659ca1505ddac3148a74899fcac7de4b
afeiship <1290657123@qq.com>
2018-05-23 09:53:12 +0800

    issue: intail

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # ushell-module-nginx
 > Unix like shell for nginx.
 
+## in Mac(edit conf):
+```shell
+cd /usr/local/etc/nginx/
+```
+
 ## resources:
 
 ## commands:
--- index.sh
+++ index.sh
@@ -5,5 +5,6 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias nginx-start="sudod nginx";
 alias nginx-stop="sudod nginx -s stop";
 alias nginx-reload="sudod nginx -s erload";
+alias nginx-t="sudod nginx -T";
 
 unset ROOT_PATH;
