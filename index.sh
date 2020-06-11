61b67145f9e49afa0605b7d43045169d8b72671c
afeiship <1290657123@qq.com>
2020-06-11 21:38:00 +0800

    fix: reload cmd

--- index.sh
+++ index.sh
@@ -4,7 +4,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias nginx-start="sudo nginx";
 alias nginx-stop="sudo nginx -s stop";
-alias nginx-reload="sudo nginx -s erload";
+alias nginx-reload="sudo nginx -s reload";
 alias nginx-t="sudo nginx -T";
 alias e-nginx="code /usr/local/etc/nginx";
 
