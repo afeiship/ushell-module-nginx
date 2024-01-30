1250b825c614b094508ad3c1389f2628abfc167b
afeiship <1290657123@qq.com>
2024-01-30 20:25:29 +0800

    chore: upt ngx for ubuntu

--- index.sh
+++ index.sh
@@ -2,19 +2,11 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias ngx-start="nginx";
-alias ngx-stop="nginx -s stop";
-alias ngx-reload="nginx -s reload";
-alias ngx-status="ps aux | grep nginx";
-alias ngx-test="nginx -t";
-
-if [[ $(uname) == Darwin ]]; then
-  source $ROOT_PATH/index_ubuntu.sh;
-fi
-
-if [ -f /etc/lsb-release ]; then
-   source $ROOT_PATH/index_ubuntu.sh;
-fi
-
+alias ngx-start="sudo nginx";
+alias ngx-stop="sudo nginx -s stop";
+alias ngx-reload="sudo nginx -s reload";
+alias ngx-status="ps aux | grep sudo nginx";
+alias ngx-t="sudo nginx -t";
+alias ngx-tt="sudo nginx -T";
 
 unset ROOT_PATH;
--- index_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-alias e-nginx="code /usr/local/etc/nginx";
